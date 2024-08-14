7bf0e78fa56ea124f873d8b17ae44dbdbdaca0a7
jok1cob <john777kurian@gmail.com>
2024-08-14 18:34:45 +0530

    Assignment 6 part 1 done

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,9 +10,22 @@
 #include <errno.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <pthread.h>
+#include <time.h>
+#include "queue.h"
+
+struct node {
+    pthread_t tid;
+    bool thread_completed;
+    int  accept_fd;
+    FILE* txt_fd;
+    SLIST_ENTRY(node) nodes;
+};
+SLIST_HEAD(slisthead, node);
 
 #define BUFF_SIZE 512
 static bool kill_app = false;
+pthread_mutex_t file_access_mtx = PTHREAD_MUTEX_INITIALIZER;
 
 void SigHandler(int signo){
     switch (signo) {
@@ -32,7 +45,71 @@ void SigHandler(int signo){
     }
 }
 
+void* handle_connection(void* arg) {
+    FILE* txtfd = NULL;
+    char* ret = NULL;
+    char buff[BUFF_SIZE] = {0};
+    int rc;
+
+    struct node* n = (struct node *)arg;
+
+    pthread_mutex_lock(&file_access_mtx);
+    txtfd = fopen("/var/tmp/aesdsocketdata", "a+");
+    n->txt_fd = txtfd;
+    do {
+        ret = NULL;   
+        rc = recv(n->accept_fd, (void*) buff, (BUFF_SIZE - 1), 0);  
+        fprintf(txtfd, "%.*s", rc, (char*)buff);
+        buff[BUFF_SIZE] = '\0'; // adding null at last since valgrind was complaining for strchr not having null termination
+        ret = strchr(buff,'\n');
+        if(ret != NULL)
+            break;            
+        else 
+            memset(buff, 0, BUFF_SIZE);
+    }while(rc > 0);
+    fflush(txtfd);
+    fclose(txtfd);
+    n->txt_fd = NULL;
+    pthread_mutex_unlock(&file_access_mtx);
+
+    pthread_mutex_lock(&file_access_mtx);
+    txtfd = fopen("/var/tmp/aesdsocketdata", "r");
+    n->txt_fd = txtfd;
+    do {
+        memset(buff, 0, BUFF_SIZE);
+        rc = fread(buff, 1, BUFF_SIZE, txtfd );            
+        send(n->accept_fd, (void*) buff, rc, 0 );
+    } while(rc == BUFF_SIZE);
+    fclose(txtfd);
+    n->txt_fd = NULL;
+    pthread_mutex_unlock(&file_access_mtx);
+    close(n->accept_fd);
+
+    n->thread_completed = true;
+    return arg;
+}
+
+void timer_thread(union sigval arg)
+{
+  FILE* txtfd = NULL;
+  char outstr[200] = {0};
+  time_t t;
+  struct tm *tmp;
+  pthread_mutex_lock(&file_access_mtx);
+  txtfd = fopen("/var/tmp/aesdsocketdata", "a+");
+  t = time(NULL);
+  tmp = localtime(&t);
+  strftime(outstr, sizeof(outstr),"%a, %d %b %Y %T %z", tmp);
+  fprintf(txtfd, "timestamp:%s\n", (char*)outstr);
+  fflush(txtfd);
+  fclose(txtfd);
+  pthread_mutex_unlock(&file_access_mtx);
+}
+
+
 int main(int argc, char* argv[]) {
+
+    struct slisthead head;
 
     bool daemon = false;
     int rc;
@@ -48,9 +125,7 @@ int main(int argc, char* argv[]) {
     struct sockaddr_storage their_addr = {0};
     socklen_t addr_size = sizeof (their_addr);
     int accept_fd = 0;
-    FILE* txtfd = NULL;
-    char* ret = NULL;
-    char buff[BUFF_SIZE] = {0};
+
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;       // use IPv4 
@@ -61,7 +136,26 @@ int main(int argc, char* argv[]) {
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
+    SLIST_INIT(&head);
+
     openlog(NULL, 0, LOG_USER);
+
+    timer_t timer_id;
+    struct itimerspec ts;
+    struct sigevent se;
+    /*
+    * Set the sigevent structure to cause the signal to be
+    * delivered by creating a new thread.
+    */
+    se.sigev_notify = SIGEV_THREAD;
+    se.sigev_value.sival_ptr = &timer_id;
+    se.sigev_notify_function = timer_thread;
+    se.sigev_notify_attributes = NULL;
+
+    ts.it_value.tv_sec =10;
+    ts.it_value.tv_nsec = 0;
+    ts.it_interval.tv_sec = 10;
+    ts.it_interval.tv_nsec = 0;
 
     if( (argc == 2) && (strcmp(argv[1],"-d") == 0) ) {
         syslog(LOG_DEBUG, "Trying to run aesdsocket as daemon");
@@ -74,8 +168,7 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "\nIncorrect options use ./aesdsocket -d to run as daemon\n");
         fprintf(stderr, "or ./aesdsocket to run as process\n");
         return 0;
-    }
-    
+    }  
 
     if ((status = getaddrinfo(NULL, "9000", &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
@@ -125,38 +218,43 @@ int main(int argc, char* argv[]) {
 
     remove("/var/tmp/aesdsocketdata");
 
+    //spawn timestamp logger thread? or timer
+    rc = timer_create(CLOCK_REALTIME, &se, &timer_id);
+    if (rc == -1)
+        perror("Create timer");
+
+    rc = timer_settime(timer_id, 0, &ts, 0);
+    if (rc == -1)
+        perror("Set timer");
+
     while(1) {
         if(kill_app)
             break;
 
         accept_fd = accept(socketfd, (struct sockaddr *)&their_addr, &addr_size);
-        syslog(LOG_DEBUG, "Accepted connection from %s", inet_ntoa(((struct sockaddr_in*)&their_addr)->sin_addr));        
+        syslog(LOG_DEBUG, "Accepted connection from %s", inet_ntoa(((struct sockaddr_in*)&their_addr)->sin_addr));    
+        
+        struct node *nn;
+        nn = calloc(sizeof(struct node), 1);
+        nn->thread_completed = false;
+        nn->accept_fd = accept_fd;
+        nn->txt_fd = NULL;
+        SLIST_INSERT_HEAD(&head, nn, nodes);
 
-        txtfd = fopen("/var/tmp/aesdsocketdata", "a+");
-        do {
-            ret = NULL;   
-            rc = recv(accept_fd, (void*) buff, (BUFF_SIZE - 1), 0);  
-            fprintf(txtfd, "%.*s", rc, (char*)buff);
-            buff[BUFF_SIZE] = '\0'; // adding null at last since valgrind was complaining for strchr not having null termination
-            ret = strchr(buff,'\n');
-            if(ret != NULL)
-                break;            
-            else 
-                memset(buff, 0, BUFF_SIZE);
-        }while(rc > 0);
-        fflush(txtfd);
-        fclose(txtfd);
+        pthread_create(&nn->tid, NULL, handle_connection, nn);
 
-        txtfd = fopen("/var/tmp/aesdsocketdata", "r");
-        do {
-            memset(buff, 0, BUFF_SIZE);
-            rc = fread(buff, 1, BUFF_SIZE, txtfd );            
-            send(accept_fd, (void*) buff, rc, 0 );
-        } while(rc == BUFF_SIZE);
-        fclose(txtfd);
-        close(accept_fd);
-        syslog(LOG_DEBUG, "Closed connection from %s", inet_ntoa(((struct sockaddr_in*)&their_addr)->sin_addr));
-    }   
+       
+        // syslog(LOG_DEBUG, "Closed connection from %s", inet_ntoa(((struct sockaddr_in*)&their_addr)->sin_addr));
+    }
+    timer_delete(timer_id);
+    struct node* n1;
+    while (!SLIST_EMPTY(&head)) {          
+        n1 = SLIST_FIRST(&head);
+        pthread_join(n1->tid, NULL);
+        SLIST_REMOVE_HEAD(&head, nodes);
+        free(n1);
+    }
+
     close(socketfd);
 
     closelog();
